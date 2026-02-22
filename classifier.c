619dd66997309b3b16230cc7249d06028e56886b
Kw0e <belleaukevan8@gmail.com>
2026-02-22 01:35:19 +0100

    Add files via upload

---
+++ classifier.c
@@ -0,0 +1,18 @@
+#include "classifier.h"
+
+ObjectType classify_object(ScanResult* result) {
+
+    if(result->ble_detected) {
+        return OBJECT_BLE_DEVICE;
+    }
+
+    if(result->rfid_detected) {
+        return OBJECT_RFID_TAG;
+    }
+
+    if(result->ir_detected) {
+        return OBJECT_REMOTE;
+    }
+
+    return OBJECT_UNKNOWN;
+}
